de6e0c2de39846848692ebf5e54816b306909739
mam3d <immam3d@gmail.com>
2022-03-19 14:34:36 +0330

    refresh&access token urls

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,5 +1,9 @@
 
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .views import (
     UserRegisterView,
     UserLoginView,
@@ -8,4 +12,6 @@ from .views import (
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
     path('login/', UserLoginView.as_view(), name="login"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
