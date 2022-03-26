dae980a640f4218ad0565236a1834034b6aa1731
mam3d <immam3d@gmail.com>
2022-03-26 14:42:45 +0430

    delete unnessary validation

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -12,11 +12,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ["username", "password", "password2"]
 
-    def validate_password(self, value):
-        if len(value) < 8:
-            raise serializers.ValidationError("password is to short")
-        return value
-
     def validate(self, data):
         if data["password"] != data["password2"]:
             raise serializers.ValidationError("passwords didn't match")
