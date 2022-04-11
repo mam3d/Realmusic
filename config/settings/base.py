daef866e03f85232a70b5e655daed4b5b1670875
mam3d <immam3d@gmail.com>
2022-04-11 10:37:01 +0430

    update dockerignore

--- config/settings/base.py
+++ config/settings/base.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = config("SECRET_KEY", cast=str)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
-
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -18,4 +21,4 @@ urlpatterns = [
     path('api/artist/', include("artist.api.urls")),
     path('api/music/', include("music.api.urls")),
     path('api/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
