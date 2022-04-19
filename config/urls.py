4cbe94fa3eab3d7a7d4d1aa9def16caaecfebe5b
mam3d <immam3d@gmail.com>
2022-04-19 14:23:48 +0430

    document is now allowany

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+
+from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -13,6 +15,7 @@ schema_view = get_schema_view(
       default_version='v1',
    ),
    public=True,
+   permission_classes=(AllowAny,),
 )
 
 urlpatterns = [
