3fac50d33c6ae8ab3eaf7334597bea92e42e2224
mam3d <immam3d@gmail.com>
2022-04-24 16:07:56 +0430

    followviews tests

--- artist/tests/factory.py
+++ artist/tests/factory.py
@@ -1,8 +1,15 @@
 from factory.django import DjangoModelFactory
-from ..models import Artist
+from ..models import Artist, Follow
 
 class ArtistFactory(DjangoModelFactory):
     name = None
     genre = None
     class Meta:
         model = Artist
+
+
+class FollowFactory(DjangoModelFactory):
+    user = None
+    artist = None
+    class Meta:
+        model = Follow
--- artist/tests/test_views.py
+++ artist/tests/test_views.py
@@ -1,8 +1,9 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from artist.models import Follow
 from user.tests.factory import UserFactory
 from music.tests.factory import GenreFactory, SongFactory
-from .factory import ArtistFactory
+from .factory import ArtistFactory, FollowFactory
 
 
 class ArtistListViewTest(APITestCase):
@@ -43,3 +44,41 @@ class ArtistDetailViewTest(APITestCase):
         self.assertEqual(response_data["name"], self.artist.name)
         self.assertEqual(response_data["albums"], [])
         self.assertEqual(self.artist.get_single_songs()[0].name, response_data["single_songs"][0]["name"])
+
+
+class FollowViewTest(APITestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.artist = ArtistFactory(name="testing2")
+        self.follow = FollowFactory(user=self.user, artist=self.artist)
+
+        access = self.user.get_jwt_token()["access"]
+        self.authorization_header = {"HTTP_AUTHORIZATION":f"Bearer {access}"}
+
+    def test_get(self):
+        url = reverse("follow")
+        response = self.client.get(url, **self.authorization_header)
+        response_data = response.data
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data[0]["artist"], self.artist.name)
+
+    def test_create(self):
+        url = reverse("follow")
+        artist = ArtistFactory(name="eminem")
+        payload = {
+            "artist":artist.id
+        }
+        response = self.client.post(url, data=payload, **self.authorization_header)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(Follow.objects.get(user=self.user, artist=artist))
+
+    def test_delete(self):
+        url = reverse("follow-delete", kwargs={"pk":int(f"{self.user.id}{self.artist.id}")})
+        response = self.client.delete(url, **self.authorization_header)
+
+        self.assertEqual(response.status_code, 204)
+        with self.assertRaises(Follow.DoesNotExist):
+            Follow.objects.get(user=self.user, artist=self.artist)
+    
