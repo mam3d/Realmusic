e4a244110c1f060973596151f69c31d5251179b0
mam3d <immam3d@gmail.com>
2022-05-09 10:56:35 +0430

    update FollowCreateSerializer to use get_or_create instead of validation and ArtistDetailView to show following status of user that is viewing the artist detail

--- artist/api/serializers.py
+++ artist/api/serializers.py
@@ -14,13 +14,21 @@ from music.api.serializers import SongListSerializer, AlbumListSerializer
 class ArtistDetailSerializer(serializers.ModelSerializer):    
     albums = AlbumListSerializer(read_only=True, many=True)
     single_songs = serializers.SerializerMethodField()
+    follow = serializers.SerializerMethodField()
     class Meta:
         model = Artist
-        fields = ["name","image", "albums", "single_songs"]
+        fields = ["name", "image", "follow", "albums", "single_songs"]
 
     def get_single_songs(self, obj):
         serializer = SongListSerializer(obj.get_single_songs(),many=True,context=self.context)
         return serializer.data
+
+    def get_follow(self, obj):
+        # returns id of follow if the viewer user followed this artist else null
+        user = self.context["request"].user
+        follow = Follow.objects.filter(user=user, artist=obj)
+        if follow:
+            return follow[0].id 
 
 
 class FollowCreateSerializer(serializers.ModelSerializer):
@@ -29,18 +37,12 @@ class FollowCreateSerializer(serializers.ModelSerializer):
         fields = ["id", "artist"]
         extra_kwargs = {"id":{"read_only":True}}
 
-    def validate(self, validated_data):
-        user = self.context["request"].user
-        artist = validated_data["artist"]
-        try:
-            Follow.objects.get(user=user, artist=artist)
-        except Follow.DoesNotExist:
-            return validated_data   
-        raise serializers.ValidationError("follow with this user and artist exists")
 
     def create(self, validated_data):
-        validated_data.update(user=self.context["request"].user)
-        return super().create(validated_data)
+        user = self.context["request"].user
+        artist = validated_data["artist"]
+        follow, created = Follow.objects.get_or_create(user=user, artist=artist)
+        return follow
 
 
 class FollowingSerializer(serializers.ModelSerializer):
